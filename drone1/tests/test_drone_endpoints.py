b4994481d52833ff9004c0ccba172d5fb24cdf78
Akshay Dahiya <xadahiya@gmail.com>
2017-07-22 00:00:12 +0530

    Update Drone tests for datastream and drone

--- drone1/tests/test_drone_endpoints.py
+++ drone1/tests/test_drone_endpoints.py
@@ -47,7 +47,7 @@ class TestDroneRequests(unittest.TestCase):
         ## 404 if drone is not initialized use mechanics.drone_init to initialize
         assert request_get.status_code in [200, 404]
         assert request_put.status_code == 405
-        assert request_post.status_code == 200
+        assert request_post.status_code in [200,201]
         assert request_delete.status_code == 405
 
     def test_request_datastream(self):
@@ -61,7 +61,7 @@ class TestDroneRequests(unittest.TestCase):
         ## 404 if drone is not initialized use mechanics.drone_init to initialize
         assert request_get.status_code in [200,404]
         assert request_put.status_code == 405
-        assert request_post.status_code == 200
+        assert request_post.status_code in [200,201]
         assert request_delete.status_code == 405
 
     def test_request_command_collection(self):
