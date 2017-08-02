35426a831692c1f6e92108d737e0be11aeb777a4
Akshay Dahiya <xadahiya@gmail.com>
2017-08-02 17:53:47 +0530

    Update init.sh to start 15 sec loop in drone, update .gitignore to ignore any db

--- init.sh
+++ init.sh
@@ -2,6 +2,9 @@ echo "Starting Simulation Central Controller"
 python sim_controller/flock_controller/main.py &
 echo "Central Controller started successfully!"
 
+echo "Initializing Central Controller Location"
+python sim_controller/flock_controller/mechanics/update_location.py
+
 echo "Starting Drone1"
 python sim_drone/flock_drone/main.py &
 echo "Drone1 successfully started!"
@@ -15,3 +18,8 @@ echo "Simulation GUI started successfully"
 
 echo "Initializing Drone1"
 python sim_drone/flock_drone/mechanics/drone_init.py
+
+## Wait for 2 seconds.
+/bin/sleep 2
+echo "Starting Listner.py"
+python sim_drone/flock_drone/mechanics/listner.py &
