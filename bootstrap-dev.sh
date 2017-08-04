03128fb2ea9e53c232e96cadad9f89c8b5bc7cd7
Akshay Dahiya <xadahiya@gmail.com>
2017-08-04 23:37:13 +0530

    update bootstrap script to use https, increase sleep time in init.sh

--- bootstrap-dev.sh
+++ bootstrap-dev.sh
@@ -5,18 +5,18 @@ echo "Install requirements..."
 pip install -r requirements.txt
 #
 
-git clone -b develop http://github.com/HTTP-APIs/hydrus.git hydrus
-git clone http://github.com/xadahiya/hydra-flock-central-controller.git sim_controller
-git clone http://github.com/xadahiya/hydra-flock-gui.git sim_gui
+git clone -b develop https://github.com/HTTP-APIs/hydrus.git hydrus
+git clone https://github.com/xadahiya/hydra-flock-central-controller.git sim_controller
+git clone https://github.com/xadahiya/hydra-flock-gui.git sim_gui
 
 # Drone1
-git clone http://github.com/xadahiya/hydra-flock-drone.git sim_drone1
+git clone https://github.com/xadahiya/hydra-flock-drone.git sim_drone1
 # Drone2
-git clone http://github.com/xadahiya/hydra-flock-drone.git sim_drone2
+git clone https://github.com/xadahiya/hydra-flock-drone.git sim_drone2
 # Drone3
-git clone http://github.com/xadahiya/hydra-flock-drone.git sim_drone3
+git clone https://github.com/xadahiya/hydra-flock-drone.git sim_drone3
 # Drone4
-git clone http://github.com/xadahiya/hydra-flock-drone.git sim_drone4
+git clone https://github.com/xadahiya/hydra-flock-drone.git sim_drone4
 
 ## Updating drone settings
 echo "Updating drone settings"
--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ python sim_controller/flock_controller/main.py &
 echo "Central Controller started successfully!"
 
 ## Wait for 2 seconds.
-/bin/sleep 4
+/bin/sleep 8
 
 
 echo "Initializing Central Controller Location"
@@ -32,7 +32,7 @@ python sim_gui/app.py &
 echo "Simulation GUI started successfully"
 
 ## Wait for 4 seconds.
-/bin/sleep 4
+/bin/sleep 8
 
 echo "Initializing Drone1"
 python sim_drone1/flock_drone/mechanics/drone_init.py
@@ -47,7 +47,7 @@ echo "Initializing Drone4"
 python sim_drone4/flock_drone/mechanics/drone_init.py
 
 ## Wait for 2 seconds.
-/bin/sleep 2
+/bin/sleep 8
 
 echo "Starting Drone1 main mechanics Loop"
 python sim_drone1/flock_drone/mechanics/listener.py &
