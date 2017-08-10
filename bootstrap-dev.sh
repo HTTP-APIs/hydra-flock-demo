b52cb61723eb60eceaacd9908208fc7f9828c00d
Akshay Dahiya <xadahiya@gmail.com>
2017-08-10 21:58:50 +0530

    updated repos, using working version of hydrus

--- bootstrap-dev.sh
+++ bootstrap-dev.sh
@@ -5,18 +5,18 @@ echo "Install requirements..."
 pip install -r requirements.txt
 #
 
-git clone -b develop https://github.com/chrizandr/hydrus.git hydrus
-git clone -b develop --single-branch https://github.com/chrizandr/hydra-flock-central-controller.git sim_controller
-git clone -b develop --single-branch https://github.com/chrizandr/hydra-flock-gui.git sim_gui
+git clone -b develop https://github.com/HTTP-APIs/hydrus.git hydrus
+git clone --single-branch https://github.com/xadahiya/hydra-flock-central-controller.git sim_controller
+git clone -b develop --single-branch https://github.com/HTTP-APIs/hydra-flock-gui.git sim_gui
 
 # Drone1
-git clone -b develop --single-branch https://github.com/chrizandr/hydra-flock-drone.git sim_drone1
+git clone --single-branch https://github.com/xadahiya/hydra-flock-drone.git sim_drone1
 # Drone2
-git clone -b develop --single-branch https://github.com/chrizandr/hydra-flock-drone.git sim_drone2
+git clone --single-branch https://github.com/xadahiya/hydra-flock-drone.git sim_drone2
 # Drone3
-git clone -b develop --single-branch https://github.com/chrizandr/hydra-flock-drone.git sim_drone3
+git clone --single-branch https://github.com/xadahiya/hydra-flock-drone.git sim_drone3
 # Drone4
-git clone -b develop --single-branch https://github.com/chrizandr/hydra-flock-drone.git sim_drone4
+git clone --single-branch https://github.com/xadahiya/hydra-flock-drone.git sim_drone4
 
 ## Updating drone settings
 echo "Updating drone settings"
